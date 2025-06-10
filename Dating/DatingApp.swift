8b19ea350ea46697d6dbcfcdd8bf69ca07f9db6d
Jackwang-lea <canikissyoui.0424@gmail.com>
2025-06-10 14:21:07 +0800

    Initial Commit

---
+++ Dating/DatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingApp.swift
+//  Dating
+//
+//  Created by zhetaoWang on 2025/6/10.
+//
+
+import SwiftUI
+
+@main
+struct DatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DatingTests/DatingTests.swift
@@ -0,0 +1,17 @@
+//
+//  DatingTests.swift
+//  DatingTests
+//
+//  Created by zhetaoWang on 2025/6/10.
+//
+
+import Testing
+@testable import Dating
+
+struct DatingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
